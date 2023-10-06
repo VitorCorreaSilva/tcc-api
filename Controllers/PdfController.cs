a057159923eec244ecbac9f00a8863c6ad392423
Vitor Correa da Silva <correa.vitor.silva@gmail.com>
2023-10-06 11:44:05 -0300

    adicionando visio

--- Controllers/PdfController.cs
+++ Controllers/PdfController.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using Google.Cloud.Vision.V1;
 using iTextSharp.text.pdf;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -24,19 +25,23 @@ namespace tcc.Controllers
         }
 
         [HttpPost]
-        public List<PdfRetorno> Anexar(IFormFile file)
+        public List<string> Anexar(IFormFile file)
         {
             var extension = Path.GetExtension(file.FileName);
             var fileName = Path.GetFileNameWithoutExtension(file.FileName);
-            PdfParser pdfParser = new PdfParser();
 
-            pdfParser.ExtractText(file.OpenReadStream(), fileName);
+            Image image = Image.FromStream(file.OpenReadStream());
 
-            List<PdfRetorno> retorno = new List<PdfRetorno>();
-            retorno.Add(new PdfRetorno { Valor=1.23, Descricao = "Pao" });
-            retorno.Add(new PdfRetorno { Valor=12.23, Descricao = "Cafe" });
+            List<string> lista = new List<string>();
 
-            return retorno;
+            ImageAnnotatorClient client = ImageAnnotatorClient.Create();
+            IReadOnlyList<EntityAnnotation> textAnnotations = client.DetectText(image);
+            foreach (EntityAnnotation text in textAnnotations)
+            {
+                lista.Add($"Description: {text.Description}");
+            }
+
+            return lista;
         }
     }
 }
